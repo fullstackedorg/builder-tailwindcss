cc19c2a13166828eedd3abc2792760a9cfd26cfe
cplepage <lepage.charlesphilippe@gmail.com>
2026-02-21 09:13:35 -0500

    small test

---
+++ test.ts
@@ -0,0 +1,23 @@
+import { build, init } from "./index.ts";
+import fs from "fs";
+import assert from "assert";
+
+fs.writeFileSync("index.html", `<h1 class="text-3xl font-bold underline bg-red-500">
+  Hello world!
+</h1>`);
+
+await init({
+    lightningcss: "node_modules/lightningcss-wasm/lightningcss_node.wasm",
+    oxide: "node_modules/@esm.sh/oxide-wasm/pkg/oxide_wasm_bg.wasm",
+    tailwindcss: "node_modules/tailwindcss"
+});
+
+const outfile = "./output.css";
+
+await build(outfile, ["./index.html"]);
+
+assert.ok(fs.existsSync(outfile))
+
+fs.rmSync("./index.html")
+fs.rmSync(outfile)
+
